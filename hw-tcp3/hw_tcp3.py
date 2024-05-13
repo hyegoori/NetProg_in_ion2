7da1f82c3e0eeaf37161b606b5a60551a7fa16ad
Hyewon Kim <117633516+hyegoori@users.noreply.github.com>
2024-05-14 03:17:50 +0900

    hw_tcp3.py

--- hw-tcp3/hw_tcp3.py
+++ hw-tcp3/hw_tcp3.py
@@ -6,7 +6,7 @@ def send_not_found(conn):
     conn.send(b'<HTML><HEAD><TITLE>Not Found</TITLE></HEAD><BODY>Not Found</BODY></HTML>')
 
 def handle_request(filename, conn):
-    # 파일 확장자에 따른 MIME 타입 설정
+    
     if filename.endswith('.html'):
         mimeType = 'text/html; charset=utf-8'  # charset=utf-8 추가
         mode = 'r'
@@ -21,7 +21,7 @@ def handle_request(filename, conn):
         send_not_found(conn)
         return
     
-    # 파일 존재 여부 확인 및 전송
+   
     try:
         with open(filename, mode, encoding=encoding if 'encoding' in locals() else None) as f:
             data = f.read()
@@ -29,9 +29,9 @@ def handle_request(filename, conn):
             conn.send('Content-Type: '.encode() + mimeType.encode() + b'\r\n')
             conn.send(b'\r\n')
             if isinstance(data, str):
-                conn.send(data.encode('utf-8'))  # UTF-8로 인코딩 변경
+                conn.send(data.encode('utf-8'))  
             else:
-                conn.send(data)  # 바이너리 파일의 경우
+                conn.send(data) 
     except FileNotFoundError:
         send_not_found(conn)
 
@@ -46,10 +46,10 @@ def main():
         c, addr = s.accept()
         data = c.recv(1024)
         msg = data.decode()
-        req = msg.split('\r\n')[0]  # 요청 라인 추출
-        filename = req.split(' ')[1][1:]  # 요청된 파일 이름 추출
+        req = msg.split('\r\n')[0] 
+        filename = req.split(' ')[1][1:]  
         
-        if filename == '':  # 기본 페이지 설정
+        if filename == '':
             filename = 'index.html'
         
         handle_request(filename, c)
